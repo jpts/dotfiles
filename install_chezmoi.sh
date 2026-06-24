2cadb0d03126330aad5711c6b4f89e2d8c6c8c66
James Cleverley-Prance <jpts@users.noreply.github.com>
2026-06-24 10:29:41 +0000

    auto install cosign

--- install_chezmoi.sh
+++ install_chezmoi.sh
@@ -4,15 +4,51 @@ set -euo pipefail
 
 VERSION="2.70.5"
 
-for cmd in mktemp curl cosign tar sha256sum install strip; do
+if command -v chezmoi &>/dev/null; then
+  echo "Chezmoi already installed" >&2
+  exit 0
+fi
+
+for cmd in mktemp curl tar sha256sum install awk; do
   if ! command -v $cmd &>/dev/null; then
     echo "Command not found: $cmd"
     exit 1
   fi
 done
 
+DISTRO="$(awk -F= '/^ID=/ {print $2}' /etc/os-release)"
+VERSION="$(awk -F= '/^VERSION=/ {print $2}' /etc/os-release)"
+
+install_cosign_from_github() {
+  curl -sSfL https://github.com/sigstore/cosign/releases/download/v3.1.1/cosign-linux-amd64 -o ./cosign
+  echo -n "ae1ecd212663f3693ad9edf8b1a183900c9a52d3155ba6e354237f9a0f6463fc cosign" | sha256sum --check -
+  install ./cosign ~/.local/bin
+}
+
 TMP=$(mktemp -d)
 pushd "$TMP"
+
+if ! command -v cosign &>/dev/null; then
+  case DISTRO in
+    "ubuntu")
+      if [[ $VERSION == "25."* || $VERSION == "26."* ]]; then
+        sudo apt install cosign -y --no-install-recommends
+      else
+        install_cosign_from_github
+      fi
+      ;;
+    "debian")
+      sudo apt install cosign -y --no-install-recommends
+      ;;
+    "alpine")
+      sudo apk add cosign -y --no-cache
+    *)
+      echo "Distro $DISTRO unsuported" >&2
+      install_cosign_from_github
+      ;;
+  esac
+fi
+
 
 cat > ./chezmoi.pub << EOF
 -----BEGIN PUBLIC KEY-----
@@ -35,7 +71,7 @@ sha256sum --check chezmoi_${VERSION}_checksums.txt --ignore-missing
 tar xf chezmoi_${VERSION}_linux_amd64.tar.gz chezmoi
 
 mkdir -p ~/.local/bin
-install -s ./chezmoi ~/.local/bin
+install ./chezmoi ~/.local/bin
 
 popd
 if [ -d "$TMP" ]; then
