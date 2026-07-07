d2c63d7b1d0faed9787ac7b8fe934f2543bca83e
jpts <jpts@users.noreply.github.com>
2026-07-07 11:25:46 +0100

    fix prompt dir colour

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -154,8 +154,7 @@
   ### dir ###
 
   # Default current directory color.
-  #typeset -g POWERLEVEL9K_DIR_FOREGROUND=31
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=$blue
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=31
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
